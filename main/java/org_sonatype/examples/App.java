f9f40ef1b405edb95aaeb72ba068164152e5771e
joshmp82 <45913717+joshmp82@users.noreply.github.com>
2019-01-10 10:57:13 +0530

    New maven project

---
+++ main/java/org_sonatype/examples/App.java
@@ -0,0 +1,13 @@
+package org_sonatype.examples;
+
+/**
+ * Hello Manoj Joshi!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Manoj Joshi!" );
+    }
+}
